71216eed2f9e7f4f688b7c41cef5552455d1eede
Matthew W <132436349+CrippledRuler@users.noreply.github.com>
2025-06-02 02:49:47 +0000

    add index

--- main.py
+++ main.py
@@ -10,8 +10,16 @@ def index ():
         ("Offensive class", "These classes are the main grunt of your team, they are meant to be the ones that are doig the dirty work. These classes are: Scout, soldier and pyro ", "Button Text", "static/images/logo.png"),
         ("Defensive class", "description", "Button Text", "static/images/logo.png"),
         ("title", "description", "Button Text", "static/images/logo.png"),
+        ("title", "description", "Button Text", "static/images/logo.png"),
+        ("title", "description", "Button Text", "static/images/logo.png"),
+        ("title", "description", "Button Text", "static/images/logo.png"),
     )
-    return render_template("index.html", cards=card_data), 200
+    return render_template("index.html", cards = card_data), 200
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html"), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
